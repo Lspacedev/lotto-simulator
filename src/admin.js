63d3cd6119a14a17e7e850159203e21eac13060e
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2024-07-09 12:36:05 +0200

    added index.html, index.js and lotto-pseudocode.pseudo

---
+++ src/admin.js
@@ -0,0 +1,23 @@
+//export admin function
+
+export function admin(tickets) {
+  //random number generator
+  function generateWinningNumbers(min, max) {
+    let arr = [];
+    while (arr.length < 6) {
+      let num = Math.floor(Math.random() * (max - min + 1)) + min;
+      if (arr.indexOf(num) === -1) {
+        arr.push(num);
+      }
+    }
+
+    return arr;
+  }
+  const drawBtn1 = document.querySelector(".draw-btn1");
+  let winningNumbers1;
+  drawBtn1.addEventListener("click", () => {
+    winningNumbers1 = generateWinningNumbers(1, 52);
+    return winningNumbers1;
+  });
+  console.log(winningNumbers1);
+}
